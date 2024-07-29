4e0853a4b4cc8032c5e4dd7d0103f069818b3f98
Enzo Pinotti <enzopinottii@gmail.com>
2024-07-29 13:55:26 -0300

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/header.css';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <h1>Bienvenidos a Mi Landing Page</h1>
-    </header>
-  );
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    return (
+        <header className="header">
+            <section className='headerSup'>
+                <img src='https://fedesagency.com/solarCaseres/logoClaroSC.png' alt='logo solar casares' />
+                <img 
+                    src='https://fedesagency.com/solarCaseres/menuHamburguesa.png' 
+                    alt='Menu de Hamburguesas solar casares'
+                    onClick={toggleMenu} 
+                    className='menu-icon' 
+                />
+            </section>
+            <section className='headerBottom'>
+                <article className='headerTextArea'>
+                    <h1 className='titleHeader'>Energía solar y soluciones hidráulicas para un futuro sostenible</h1>
+                    <p className='parrafoHeader'>Descubrí nuestros productos innovadores y eficientes</p>
+                </article>
+                <article className='buttonsContainer'>
+                    <button className='buttonProduct'>Ver productos</button>
+                    <button className='buttonInfo'>Solicitar información</button>
+                </article>
+                <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
+                <section className='headerSup'>
+                    <img src='https://fedesagency.com/solarCaseres/logoOscuroSC.png' alt='logo solar casares' />
+                    <img 
+                        src='https://fedesagency.com/solarCaseres/menuHamburguesaOscuro.png' 
+                        alt='Menu de Hamburguesas solar casares'
+                        onClick={toggleMenu} 
+                        className='menu-icon' 
+                    />
+                </section>
+                    <ul>
+                    <li><a href="#about">Sobre nosotros</a></li>
+                    <li><a href="#products">Productos</a></li>
+                    <li><a href="#testimonials">Testimonios</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                    <li><button className='quote-button'>Solicitá una cotización</button></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+    );
 };
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,24 @@ import '../styles/main.css';
 const Main = () => {
   return (
     <main className="main-content">
-      <p>Esta es una descripción de mi producto o servicio.</p>
-
+      <section className='nosotrosContainer'>
+        <article className='textArea'>
+          <h2 className='titleNosotros'>¿Por qué nosotros?</h2>
+          <p className='parrafoNosotros'>Nos especializamos en la instalación de paneles solares de alta calidad para maximizar la producción de energía limpia y sostenible.Detrás de cada instalación, se encuentra un gran equipo comprometido hacia un futuro verde y saludable para las próximas generaciones. En Solar Casares, creemos firmemente que la energía solar no solamente ilumina hogares, sino también el camino hacia un futuro más sostenible.</p>
+          <p className='parrafoNosotros'><strong>Nuestra misión:</strong> Impulsar el desarrollo sostenible a través de las energías renovables.</p>
+        </article>
+        
+        <article className='iconsContainer'>
+          <div className='iconos1'>
+            <img alt="" src='https://fedesagency.com/solarCaseres/iconoEnergia2.png' />
+            <img alt="" src='https://fedesagency.com/solarCaseres/iconoTecnologia2.png' />
+          </div>
+          <div className='iconos2'>
+            <img alt="" src='https://fedesagency.com/solarCaseres/iconoSoporte2.png' />
+            <img alt="" src='https://fedesagency.com/solarCaseres/iconoInstalacion2.png' />
+          </div>
+        </article>
+      </section>
     </main>
   );
 };
