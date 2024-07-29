afc1373ff7aaeee04391aea01b5f36bd66b78239
Enzo Pinotti <enzopinottii@gmail.com>
2024-07-29 09:45:21 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Bienvenidos a Mi Landing Page</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/main.css';
+
+const Main = () => {
+  return (
+    <main className="main-content">
+      <p>Esta es una descripción de mi producto o servicio.</p>
+
+    </main>
+  );
+};
+
+export default Main;
